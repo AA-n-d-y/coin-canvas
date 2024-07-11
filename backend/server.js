f204e42bc602c8d3bf8db7412f0cb5fd988118d2
Andy <andyhuang9338@gmail.com>
2024-07-10 19:24:59 -0700

    features: users can now create accounts

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@
 
 /// Setup
 require('dotenv').config(); // Load environment variables from .env
-
 let PORT = process.env.PORT;
 const express = require("express");
 const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
 // Connect to MongoDB using async/await
 let DB_URL = process.env.DB_URL;
@@ -29,22 +31,28 @@ const User = require("./User.js");
 /// Routes
 
 // Post request (creating a new account)
-app.post("/register", async (request, response, next) => { 
+app.post("/register", async (request, response) => { 
   // Extracting the details
   const { firstName, lastName, email, username, password } = request.body;
 
-  // If the user already exists, do not create an account
-  const tempUser = await User.findOne({username: username});
-  if (tempUser == null) {
-    response.status(409).json({createdUser: false});
-    return;
-  }
+  try {
+    // If the user already exists, do not create an account
+    const tempUser = await User.findOne({username: username});
+    if (tempUser != null) {
+      response.status(409).json({createdUser: false});
+      return;
+    }
 
-  // Otherwise, create a new account and save it to the database
-  const newUser = new User({firstName, lastName, email, username, password});
-  await newUser.save();
-  response.status(201).json({createdUser: true, newUser});
-})
+    // Otherwise, create a new account and save it to the database
+    const newUser = new User({firstName, lastName, email, username, password});
+    await newUser.save();
+    response.status(201).json({createdUser: true, newUser});
+  }
+  
+  catch(error) {
+    console.error("Error in creating an account", error);
+  }
+});
 
 
 /// Running the server
